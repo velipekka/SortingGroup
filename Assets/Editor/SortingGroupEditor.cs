e3ff64b826666a476f90dff6c4ca6f7cf74bbc00
Veli-Pekka Kokkonen <veli@unity3d.com>
2015-01-28 02:46:41 +0200

    SortingGroup component and editor added

---
+++ Assets/Editor/SortingGroupEditor.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Reflection;
+using UnityEngine;
+using UnityEditorInternal;
+
+namespace UnityEditor
+{
+	[CustomEditor (typeof (SortingGroup))]
+	internal class SortingGroupEditor : Editor
+	{
+		ReorderableList list;
+		SerializedProperty sortingLayerID;
+		SerializedProperty groupOrder;
+		protected static MethodInfo meth_SortingLayerField;
+
+		static Vector2 scrollArea;
+
+		void OnEnable()
+		{
+			var sortingGroup = target as SortingGroup;
+			sortingGroup.Refresh ();
+
+			list = new ReorderableList(serializedObject,
+				serializedObject.FindProperty("rendererInfos"),
+				true, false, false, false);
+
+			sortingLayerID = serializedObject.FindProperty("sortingLayerID");
+			groupOrder = serializedObject.FindProperty ("groupOrder");
+
+			// Get SortingLayerField method
+			var editorTypes = typeof(Editor).Assembly.GetTypes();		
+			var type = editorTypes.FirstOrDefault(t => t.Name == "EditorGUILayout");
+			meth_SortingLayerField = type.GetMethod("SortingLayerField",(BindingFlags.Static | BindingFlags.NonPublic));
+		}
+
+		public override void OnInspectorGUI()
+		{
+			var sortingGroup = target as SortingGroup;
+
+			serializedObject.Update();
+
+			scrollArea = GUILayout.BeginScrollView (scrollArea);
+			list.DoLayoutList();
+			GUILayout.EndScrollView();
+
+			EditorGUI.BeginChangeCheck();
+			SortingLayerField (new GUIContent("Sorting Layer"), sortingLayerID, EditorStyles.popup);
+			if (EditorGUI.EndChangeCheck())
+			{
+				foreach (var renderer in sortingGroup.GetComponentsInChildren<Renderer>())
+				{
+					var so = new SerializedObject(renderer);
+					so.FindProperty("m_SortingLayerID").intValue = sortingLayerID.intValue;
+					so.ApplyModifiedProperties();
+				}
+			}
+
+			EditorGUILayout.PropertyField (groupOrder);
+			serializedObject.ApplyModifiedProperties();
+		}
+
+		static void SortingLayerField(GUIContent label, SerializedProperty layerID, GUIStyle style)
+		{
+			var parameters = new object[]{label,layerID,style};
+			meth_SortingLayerField.Invoke(null,parameters);
+		}
+	}
+}
---
+++ Assets/SortingGroup.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class SortingGroup : MonoBehaviour
+{
+	[System.Serializable]
+	public struct RendererInfo
+	{
+		public string name;
+		public Renderer renderer;
+	}
+	public List<RendererInfo> rendererInfos = new List<RendererInfo> ();
+	public int sortingLayerID;
+	public int groupOrder;
+
+	void Reset()
+	{
+		Refresh();
+	}
+
+	bool Contains(Renderer renderer)
+	{
+		foreach (var rendererInfo in rendererInfos)
+		{
+			if (rendererInfo.renderer == null)
+			{
+				rendererInfos.Remove(rendererInfo);
+				continue;
+			}
+
+			if (rendererInfo.renderer == renderer)
+				return true;
+		}
+		return false;
+	}
+
+	public void Refresh ()
+	{
+		var renderers = new List<Renderer>(GetComponentsInChildren<Renderer>());
+
+		// Newly added renderers
+		foreach (var renderer in renderers)
+			if (!Contains(renderer))
+				rendererInfos.Add (new RendererInfo () { name = renderer.name, renderer = renderer });
+	}
+
+	void Update()
+	{
+		int index = rendererInfos.Count;
+		foreach (var renderer in rendererInfos)
+		{
+			renderer.renderer.sortingOrder = index--;
+		}
+	}
+}
